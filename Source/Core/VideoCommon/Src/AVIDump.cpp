aee6c404b5c7a643fff7cbb230e12f12dcc8bf10
goyuken <goyuken@gmail.com>
2012-08-24 09:05:01 -0400

    hopefully fix some problems with CFR mode and pal60

--- Source/Core/VideoCommon/Src/AVIDump.cpp
+++ Source/Core/VideoCommon/Src/AVIDump.cpp
@@ -11,6 +11,7 @@
 #include "VideoConfig.h"
 
 #include "..\..\AudioCommon\Src\AudioCommon.h" // for m_DumpAudioToAVI
+#include "..\..\Core\Src\ConfigManager.h" // for EuRGB60
 
 #ifdef _WIN32
 
@@ -38,6 +39,7 @@ int m_samplesSound;
 PAVISTREAM m_stream;
 PAVISTREAM m_streamCompressed;
 PAVISTREAM m_streamSound;
+int framerate;
 
 AVISTREAMINFO m_header;
 AVISTREAMINFO m_soundHeader;
@@ -56,6 +58,11 @@ bool AVIDump::Start(HWND hWnd, int w, int h)
 	m_height = h;
 
 	m_frameCountNoSplit = 0;
+
+	if (SConfig::GetInstance().m_SYSCONF->GetData<u8>("IPL.E60"))
+		framerate = 60; // always 60, for either pal60 or ntsc
+	else
+		framerate = VideoInterface::TargetRefreshRate; // 50 or 60, depending on region
 
 	if (!ac_Config.m_DumpAudioToAVI)
 	{
@@ -436,7 +443,7 @@ void AVIDump::AddFrame(const u8* data, int w, int h)
 		u64 now = CoreTiming::GetTicks ();
 		fprintf (fff, "%I64u,", now);
 
-		u64 oneCFR = SystemTimers::GetTicksPerSecond () / VideoInterface::TargetRefreshRate;
+		u64 oneCFR = SystemTimers::GetTicksPerSecond () / framerate;
 
 		fprintf (fff, "%I64u,", oneCFR);
 
@@ -503,7 +510,7 @@ bool AVIDump::SetVideoFormat()
 	memset(&m_header, 0, sizeof(m_header));
 	m_header.fccType = streamtypeVIDEO;
 	m_header.dwScale = 1;
-	m_header.dwRate = VideoInterface::TargetRefreshRate;
+	m_header.dwRate = framerate;
 	m_header.dwSuggestedBufferSize  = m_bitmap.biSizeImage;
 
 	return SUCCEEDED(AVIFileCreateStream(m_file, &m_stream, &m_header));
